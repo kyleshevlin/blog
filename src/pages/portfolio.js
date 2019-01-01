5dca60052117ec8480616853252d89895b52f7d5
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2019-01-01 13:07:22 -0800

    adds portfolio page

---
+++ src/pages/portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const Portfolio = () => (
+  <Layout>
+    <SEO title="Portfolio" keywords={['Portfolio', 'Kyle Shevlin']} />
+    <h1>Portfolio</h1>
+  </Layout>
+)
+
+export default Portfolio
