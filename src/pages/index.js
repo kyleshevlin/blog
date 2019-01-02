e53ab73ed50814500a7e5b4b8c57ca047ae29489
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2019-01-01 21:29:14 -0800

    add a query for posts for the home page to make a blog list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,53 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={['Kyle Shevlin']} />
-    <h1>Home Page</h1>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const posts = data.allFile.edges.map(edge => edge.node.childMarkdownRemark)
+
+  return (
+    <Layout>
+      <SEO title="Home" keywords={['Kyle Shevlin']} />
+      <div>
+        {posts.map(post => {
+          const {
+            html,
+            frontmatter: { date, slug, subtitle, title }
+          } = post
+
+          return (
+            <div key={slug}>
+              <div>{date}</div>
+              <h3>{title}</h3>
+              {subtitle && <h4>{subtitle}</h4>}
+              <div dangerouslySetInnerHTML={{ __html: html }} />
+            </div>
+          )
+        })}
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query HomePageQuery {
+    allFile(filter: { sourceInstanceName: { eq: "posts" } }) {
+      edges {
+        node {
+          childMarkdownRemark {
+            html
+            frontmatter {
+              title
+              slug
+              date
+              subtitle
+            }
+          }
+        }
+      }
+    }
+  }
+`
