147d4cae76bf67e560c3a3924dbf35d1537f4ef5
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2019-01-01 21:01:29 -0800

    update the favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,13 +34,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Kyle Shevlin`,
+        short_name: `kyleshevlin`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        // This path is relative to the root of the site.
+        icon: `src/images/beard-favicon.png`
       }
     },
     `gatsby-transformer-remark`
