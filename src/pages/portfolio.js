cf8ce380bc263518e97dd0f4161c1466f5ebfd51
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2019-01-01 21:33:51 -0800

    adds query for portfolio items to the portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,12 +1,48 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const Portfolio = () => (
-  <Layout>
-    <SEO title="Portfolio" keywords={['Portfolio', 'Kyle Shevlin']} />
-    <h1>Portfolio</h1>
-  </Layout>
-)
+const Portfolio = ({ data }) => {
+  const items = data.allFile.edges.map(edge => edge.node.childMarkdownRemark)
+
+  return (
+    <Layout>
+      <SEO title="Portfolio" keywords={['Portfolio', 'Kyle Shevlin']} />
+      <h1>Portfolio</h1>
+      {items.map(item => {
+        const {
+          html,
+          frontmatter: { slug, title }
+        } = item
+
+        return (
+          <div key={slug}>
+            <h3>{title}</h3>
+          </div>
+        )
+      })}
+    </Layout>
+  )
+}
 
 export default Portfolio
+
+export const query = graphql`
+  query PortfolioItemsQuery {
+    allFile(filter: { sourceInstanceName: { eq: "portfolio" } }) {
+      edges {
+        node {
+          childMarkdownRemark {
+            html
+            frontmatter {
+              title
+              slug
+              subtitle
+            }
+          }
+        }
+      }
+    }
+  }
+`
