ac45fa920619d99bf35a06cc05535efdbcfda09a
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2019-01-06 21:44:53 -0800

    update podcast page

--- src/pages/podcast.js
+++ src/pages/podcast.js
@@ -10,15 +10,15 @@ const Podcast = () => (
     />
     <h1>Podcast</h1>
     <p>
-      In September of 2017, I came up with the idea for a new podcast that would
-      find developers who had made big career changes to software engineering
-      and wanted to share the lessons they learned along the way. Out of this
-      idea, <a href="https://secondcareerdevs.com">Second Career Devs</a> was
-      born.
-    </p>
-    <p>
-      The first episode released in November, and currently there are 10
-      interviews to listen to. There are plans to do more in the fall of 2018.
+      In September of 2017, I came up with the idea for a new podcast. People
+      often asked me about my career change from pastor to programming. I
+      realized that others might benefit from listening to similar stories. So I
+      set out to find great stories of people making career changes to web
+      development and software engineering. My favorite part is finding and
+      sharing those lessons that could have only been learned by following the
+      alternative path they took. Thus,{' '}
+      <a href="https://secondcareerdevs.com">Second Career Devs</a> was born,
+      and I'm so glad to share it with you.
     </p>
     <p>
       Be sure to check it out at{' '}
