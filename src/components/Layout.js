c2adcc86e48b981b666c0cc4f4d4262ac6590111
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2019-01-06 22:21:27 -0800

    adds a component to promote my Egghead course

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import styled from '@emotion/styled'
 import { StaticQuery, graphql } from 'gatsby'
 import Container from './Container'
+import ValueSell from './ValueSell'
 import Footer from './Footer'
 import Header from './Header'
 import { bs } from '../shevy'
@@ -12,6 +13,7 @@ import Typography from '../styles/Typography'
 
 const MainWrap = styled.main`
   padding-bottom: ${bs(2)};
+  min-height: 80vh;
 `
 
 const Layout = ({ children }) => (
@@ -39,6 +41,7 @@ const Layout = ({ children }) => (
           <MainWrap role="main">
             <Container>{children}</Container>
           </MainWrap>
+          <ValueSell />
           <Footer />
         </Fragment>
       )
---
+++ src/components/ValueSell.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import { graphql, StaticQuery } from 'gatsby'
+import styled from '@emotion/styled'
+import { BREAKPOINTS, COLORS } from '../constants'
+import { bs } from '../shevy'
+import { createMediaQuery } from '../utils'
+import Container from './Container'
+
+const Wrap = styled.div`
+  background-color: ${COLORS.lightGray};
+  padding-top: ${bs(2)};
+  padding-bottom: ${bs(2)};
+`
+
+const Inner = styled.div`
+  ${createMediaQuery(BREAKPOINTS.alpha)} {
+    display: grid;
+    grid-gap: ${bs()};
+    grid-template-columns: 1fr 3fr;
+  }
+`
+
+const ImageWrap = styled.div`
+  margin-bottom: ${bs()};
+
+  ${createMediaQuery(BREAKPOINTS.alpha)} {
+    margin-bottom: 0;
+  }
+`
+
+const Image = styled.img`
+  display: block;
+  width: 100%;
+  transition: opacity 0.3s ease;
+
+  &:hover {
+    opacity: 0.85;
+  }
+`
+
+const Content = styled.div`
+  text-align: center;
+
+  ${createMediaQuery(BREAKPOINTS.alpha)} {
+    text-align: left;
+  }
+`
+
+const ValueSell = () => (
+  <StaticQuery
+    query={graphql`
+      query EggheadCourseImage {
+        allImageSharp(filter: { original: { src: { regex: "/DSA/" } } }) {
+          edges {
+            node {
+              id
+              original {
+                src
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      const { src } = data.allImageSharp.edges[0].node.original
+      const eggheadUrl =
+        'https://egghead.io/courses/data-structures-and-algorithms-in-javascript'
+
+      return (
+        <Wrap>
+          <Container>
+            <Inner>
+              <ImageWrap>
+                <a href={eggheadUrl}>
+                  <Image
+                    src={src}
+                    alt="Data Structures and Algorithms in JavaScript Logo"
+                  />
+                </a>
+              </ImageWrap>
+              <Content>
+                <h2>Check out my latest course on Egghead!</h2>
+                <a href={eggheadUrl}>
+                  <h3>Data Structures and Algorithms in JavaScript</h3>
+                </a>
+              </Content>
+            </Inner>
+          </Container>
+        </Wrap>
+      )
+    }}
+  />
+)
+
+export default ValueSell
