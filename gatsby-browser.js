382f039eac7854d9424bfb1400a31d8e1f1d0f54
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2019-01-09 22:48:20 -0800

    move global styles to wrapRootElement to try and avoid FOUC

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,10 +5,18 @@
  */
 
 // You can delete this file if you're not using it
-import React from 'react'
+import React, { Fragment } from 'react'
 import Layout from './src/components/Layout'
+import Styles from './src/styles'
 require('prismjs/themes/prism-tomorrow.css')
 
 export const wrapPageElement = ({ element, props }) => (
   <Layout {...props}>{element}</Layout>
+)
+
+export const wrapRootElement = ({ element }) => (
+  <Fragment>
+    <Styles />
+    <Fragment>{element}</Fragment>
+  </Fragment>
 )
