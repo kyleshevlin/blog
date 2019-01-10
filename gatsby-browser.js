db3a8d1532b7e02673f81c7803d3d3db4af0df65
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2019-01-09 23:45:33 -0800

    Revert "move global styles to wrapRootElement to try and avoid FOUC"
    This reverts commit 382f039eac7854d9424bfb1400a31d8e1f1d0f54.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,18 +5,10 @@
  */
 
 // You can delete this file if you're not using it
-import React, { Fragment } from 'react'
+import React from 'react'
 import Layout from './src/components/Layout'
-import Styles from './src/styles'
 require('prismjs/themes/prism-tomorrow.css')
 
 export const wrapPageElement = ({ element, props }) => (
   <Layout {...props}>{element}</Layout>
-)
-
-export const wrapRootElement = ({ element }) => (
-  <Fragment>
-    <Styles />
-    <Fragment>{element}</Fragment>
-  </Fragment>
 )
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,39 +7,57 @@ import ValueSell from './ValueSell'
 import Footer from './Footer'
 import Header from './Header'
 import { bs } from '../shevy'
+import FontFaces from '../styles/FontFaces'
+import Reset from '../styles/Reset'
+import Tags from '../styles/Tags'
+import Tweets from '../styles/Tweets'
+import Typography from '../styles/Typography'
 
 const MainWrap = styled.main`
   padding-bottom: ${bs(2)};
   min-height: 65vh;
 `
 
+const Styles = () => (
+  <Fragment>
+    <Reset />
+    <FontFaces />
+    <Tags />
+    <Typography />
+    <Tweets />
+  </Fragment>
+)
+
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-            subTitle
+  <Fragment>
+    <Styles />
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+              subTitle
+            }
           }
         }
-      }
-    `}
-    render={data => {
-      const { subTitle, title } = data.site.siteMetadata
+      `}
+      render={data => {
+        const { subTitle, title } = data.site.siteMetadata
 
-      return (
-        <Fragment>
-          <Header subTitle={subTitle} title={title} />
-          <MainWrap role="main">
-            <Container>{children}</Container>
-          </MainWrap>
-          <ValueSell />
-          <Footer />
-        </Fragment>
-      )
-    }}
-  />
+        return (
+          <Fragment>
+            <Header subTitle={subTitle} title={title} />
+            <MainWrap role="main">
+              <Container>{children}</Container>
+            </MainWrap>
+            <ValueSell />
+            <Footer />
+          </Fragment>
+        )
+      }}
+    />
+  </Fragment>
 )
 
 Layout.propTypes = {
