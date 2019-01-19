b912fb1773d8eb71e6c8104cb8667f8b80b85cfe
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2019-01-18 16:54:00 -0800

    PostAuthor no longer uses styled components but css props

--- src/components/PostAuthor.js
+++ src/components/PostAuthor.js
@@ -1,38 +1,8 @@
 import React from 'react'
 import { graphql, StaticQuery } from 'gatsby'
-import styled from '@emotion/styled'
 import { bs } from '../shevy'
 import { createMediaQuery } from '../utils'
 import { BREAKPOINTS } from '../constants'
-
-const Wrap = styled.div`
-  padding-left: ${bs()};
-  padding-right: ${bs()};
-  margin-top: ${bs(4)};
-  margin-bottom: ${bs(2)};
-
-  ${createMediaQuery(BREAKPOINTS.alpha)} {
-    display: grid;
-    grid-template-columns: 1fr 3fr;
-    grid-gap: ${bs()};
-    align-items: center;
-  }
-`
-
-const Image = styled.img`
-  display: block;
-  width: 100%;
-  max-width: 180px;
-  height: auto;
-  border-radius: 50%;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: ${bs()};
-
-  ${createMediaQuery(BREAKPOINTS.alpha)} {
-    margin-bottom: 0;
-  }
-`
 
 const query = graphql`
   query {
@@ -57,13 +27,41 @@ const PostAuthor = () => (
   <StaticQuery
     query={query}
     render={({ allImageSharp, site }) => (
-      <Wrap>
-        <Image
+      <div
+        css={{
+          paddingLeft: bs(),
+          paddingRight: bs(),
+          marginTop: bs(4),
+          marginBottom: bs(2),
+
+          [createMediaQuery(BREAKPOINTS.alpha)]: {
+            display: 'grid',
+            gridTemplateColumns: '1fr 3fr',
+            gridGap: bs(),
+            alignItems: 'center'
+          }
+        }}
+      >
+        <img
+          css={{
+            display: 'block',
+            width: '100%',
+            maxWidth: '180px',
+            height: 'auto',
+            borderRadius: '50%',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            marginBottom: bs(),
+
+            [createMediaQuery(BREAKPOINTS.alpha)]: {
+              marginBottom: 0
+            }
+          }}
           src={allImageSharp.edges[0].node.original.src}
           alt="Kyle Shevlin's face, which is mostly a beard with eyes"
         />
         <div>{site.siteMetadata.description}</div>
-      </Wrap>
+      </div>
     )}
   />
 )
