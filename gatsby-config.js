595e601bae0a03dfa1dad307297ae2d55ae308ba
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2019-04-16 09:49:43 -0700

    see if this fixes odd issue where page loads incorrectly if a trailing slash is used on the URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-remove-trailing-slashes`,
     'gatsby-transformer-json',
     {
       resolve: `gatsby-source-filesystem`,
