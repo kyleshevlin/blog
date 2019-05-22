d0c04917466edb9cfd2ae94f436eadf8d3d2aba7
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2019-05-22 08:07:03 -0700

    footnote closes on esc and outside click

--- src/components/Footnotes.js
+++ src/components/Footnotes.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useContext, useEffect, useReducer, useRef } from 'react'
 import Container from './Container'
 import { bs } from '../shevy'
 import { COLORS, FONTS } from '../constants'
@@ -28,7 +28,10 @@ const reducer = (state, action) => {
   }
 }
 
-const FootnotesContext = React.createContext(initialState)
+const FootnotesContext = React.createContext({
+  ...initialState,
+  updateFootnote: () => {}
+})
 
 export function FootnotesContainer({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState)
@@ -48,6 +51,10 @@ export function FootnotesContainer({ children }) {
 }
 
 export function FootnoteMarker({ index, content }) {
+  const { index: contextIndex, isVisible, updateFootnote } = useContext(
+    FootnotesContext
+  )
+
   if (!index) {
     throw new Error('This marker needs an index defined')
   }
@@ -57,121 +64,146 @@ export function FootnoteMarker({ index, content }) {
   }
 
   return (
-    <FootnotesContext.Consumer>
-      {({ index: contextIndex, isVisible, updateFootnote }) => (
-        <button
-          css={{
-            display: 'inline-block',
-            width: 20,
-            height: 20,
-            backgroundColor: COLORS.teal,
-            color: COLORS.white,
-            fontSize: '12px',
-            fontFamily: FONTS.catamaran,
-            lineHeight: 0,
-            border: 'none',
-            borderRadius: '50%',
-            position: 'relative',
-            top: '-2px',
-            transition: 'background-color .3s ease',
+    <button
+      css={{
+        display: 'inline-block',
+        width: 20,
+        height: 20,
+        backgroundColor: COLORS.teal,
+        color: COLORS.white,
+        fontSize: '12px',
+        fontFamily: FONTS.catamaran,
+        lineHeight: 0,
+        border: 'none',
+        borderRadius: '50%',
+        position: 'relative',
+        top: '-2px',
+        transition: 'background-color .3s ease',
 
-            '&:hover': {
-              backgroundColor: lighten(0.1, COLORS.teal)
-            },
+        '&:hover': {
+          backgroundColor: lighten(0.1, COLORS.teal)
+        },
 
-            '&:focus': {
-              outline: 'none',
-              boxShadow: `0 0 3px 1px ${darken(0.15, COLORS.teal)}`
-            }
-          }}
-          onClick={() => {
-            updateFootnote({
-              content,
-              index,
-              // If the user clicks the same marker, close the footnote
-              isVisible: isVisible && index === contextIndex ? false : true
-            })
-          }}
-        >
-          <span css={{ position: 'relative', top: '-1px' }}>{index}</span>
-        </button>
-      )}
-    </FootnotesContext.Consumer>
+        '&:focus': {
+          outline: 'none',
+          boxShadow: `0 0 3px 1px ${darken(0.15, COLORS.teal)}`
+        }
+      }}
+      onClick={() => {
+        updateFootnote({
+          content,
+          index,
+          // If the user clicks the same marker, close the footnote
+          isVisible: isVisible && index === contextIndex ? false : true
+        })
+      }}
+    >
+      <span css={{ position: 'relative', top: '-1px' }}>{index}</span>
+    </button>
   )
 }
 
 export function FootnoteDisplay() {
-  return (
-    <FootnotesContext.Consumer>
-      {({ content, index, isVisible, updateFootnote }) =>
-        isVisible ? (
+  const displayElement = useRef(null)
+  const { content, index, isVisible, updateFootnote } = useContext(
+    FootnotesContext
+  )
+
+  function handleOutsideClick(e) {
+    if (!isVisible) {
+      return
+    }
+
+    if (displayElement.current && !displayElement.current.contains(e.target)) {
+      updateFootnote({ isVisible: false })
+    }
+  }
+
+  function handleEscKey(e) {
+    if (e.key === 'Escape') {
+      updateFootnote({ isVisible: false })
+    }
+  }
+
+  useEffect(
+    () => {
+      document.addEventListener('click', handleOutsideClick)
+      document.addEventListener('keydown', handleEscKey)
+
+      return () => {
+        document.removeEventListener('click', handleOutsideClick)
+        document.removeEventListener('keydown', handleEscKey)
+      }
+    },
+    [isVisible]
+  )
+
+  return isVisible ? (
+    <div
+      ref={displayElement}
+      css={{
+        width: '100%',
+        backgroundColor: COLORS.white,
+        position: 'fixed',
+        bottom: 0,
+        borderTop: `4px solid ${COLORS.teal}`,
+        boxShadow: '0 -4px 6px rgba(0, 0, 0, .15)',
+        paddingTop: bs(),
+        paddingBottom: bs(2)
+      }}
+    >
+      <Container>
+        <div
+          css={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginBottom: bs(1)
+          }}
+        >
           <div
             css={{
-              width: '100%',
-              backgroundColor: COLORS.white,
-              position: 'fixed',
-              bottom: 0,
-              borderTop: `4px solid ${COLORS.teal}`,
-              boxShadow: '0 -4px 6px rgba(0, 0, 0, .15)',
-              paddingTop: bs(),
-              paddingBottom: bs(2)
+              display: 'inline-block',
+              width: 20,
+              height: 20,
+              backgroundColor: COLORS.teal,
+              color: COLORS.white,
+              fontSize: '12px',
+              fontFamily: FONTS.catamaran,
+              lineHeight: 1,
+              border: 'none',
+              borderRadius: '50%',
+              textAlign: 'center'
             }}
           >
-            <Container>
-              <div
-                css={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  marginBottom: bs(1)
-                }}
-              >
-                <div
-                  css={{
-                    display: 'inline-block',
-                    width: 20,
-                    height: 20,
-                    backgroundColor: COLORS.teal,
-                    color: COLORS.white,
-                    fontSize: '12px',
-                    fontFamily: FONTS.catamaran,
-                    lineHeight: 1,
-                    border: 'none',
-                    borderRadius: '50%',
-                    textAlign: 'center'
-                  }}
-                >
-                  <span css={{ position: 'relative', top: 2 }}>{index}</span>
-                </div>
-                <button
-                  css={{
-                    display: 'inline-block',
-                    backgroundColor: COLORS.teal,
-                    color: COLORS.white,
-                    fontFamily: FONTS.catamaran,
-                    fontSize: '.75em',
-                    textTransform: 'uppercase',
-                    lineHeight: 1,
-                    padding: `${bs(0.25)} ${bs(0.5)}`,
-                    border: 'none',
-                    borderRadius: '4px',
-                    transition: 'background-color .3s ease',
-
-                    '&:hover': {
-                      backgroundColor: lighten(0.1, COLORS.teal)
-                    }
-                  }}
-                  onClick={() => {
-                    updateFootnote(initialState)
-                  }}
-                >
-                  <span css={{ position: 'relative', top: '-1px' }}>Close</span>
-                </button>
-              </div>
-              <div dangerouslySetInnerHTML={{ __html: content }} />
-            </Container>
+            <span css={{ position: 'relative', top: 2 }}>{index}</span>
           </div>
-        ) : null
-      }
-    </FootnotesContext.Consumer>
-  )
+          <button
+            css={{
+              display: 'inline-block',
+              backgroundColor: COLORS.teal,
+              color: COLORS.white,
+              fontFamily: FONTS.catamaran,
+              fontSize: '.75em',
+              textTransform: 'uppercase',
+              lineHeight: 1,
+              padding: `${bs(0.25)} ${bs(0.5)}`,
+              border: 'none',
+              borderRadius: '4px',
+              transition: 'background-color .3s ease',
+
+              '&:hover': {
+                backgroundColor: lighten(0.1, COLORS.teal)
+              }
+            }}
+            onClick={() => {
+              updateFootnote(initialState)
+            }}
+          >
+            <span css={{ position: 'relative', top: '-1px' }}>Close</span>
+          </button>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </Container>
+    </div>
+  ) : null
 }
