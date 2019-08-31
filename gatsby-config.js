aae624e0acb5b2056628cc28c74e451f5b72e924
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2019-08-31 12:12:51 -0700

    add index name to algolia plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,7 @@ module.exports = {
       options: {
         appId: process.env.GATSBY_ALGOLIA_APP_ID,
         apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
         queries,
         chunkSize: 10000
       }
