8af1f77a52edefb5e17c0243eccea519e71fa8d1
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2019-10-24 08:59:11 -0700

    handle responsiveness a little bit better

--- src/components/AnnouncementBanner.js
+++ src/components/AnnouncementBanner.js
@@ -16,33 +16,42 @@ export default function AnnouncementBanner() {
       }}
     >
       <Container>
-        Attend my live, online workshop about State Machines on Nov. 13th. Get
-        your ticket now!
-        <OutboundLink
+        <div
           css={{
-            backgroundColor: COLORS.lightGray,
-            borderRadius: 4,
-            color: COLORS.teal,
-            display: 'inline-block',
-            fontSize: '.85rem',
-            marginLeft: bs(0.5),
-            padding: `${bs(0.125)} ${bs(0.25)}`,
-            transition: 'all 0.3s ease',
-
-            '&:hover': {
-              backgroundColor: COLORS.white,
-              color: COLORS.teal,
-              transform: 'translateY(-1px)'
-            },
-
-            '&:active': {
-              transform: 'translateY(0)'
-            }
+            alignItems: 'center',
+            display: 'flex'
           }}
-          href="https://ti.to/egghead-live-online-events/introduction-to-state-machines-using-xstate-with-kyle-shevlin-2019-11-13"
         >
-          Get Ticket
-        </OutboundLink>
+          <div css={{ marginRight: bs(0.5) }}>
+            Attend my live, online workshop about State Machines on Nov. 13th.
+            Get your ticket now!
+          </div>
+          <OutboundLink
+            css={{
+              backgroundColor: COLORS.lightGray,
+              borderRadius: 4,
+              color: COLORS.teal,
+              display: 'inline-block',
+              flexShrink: 0,
+              fontSize: '.85rem',
+              padding: `${bs(0.125)} ${bs(0.25)}`,
+              transition: 'all 0.3s ease',
+
+              '&:hover': {
+                backgroundColor: COLORS.white,
+                color: COLORS.teal,
+                transform: 'translateY(-1px)'
+              },
+
+              '&:active': {
+                transform: 'translateY(0)'
+              }
+            }}
+            href="https://ti.to/egghead-live-online-events/introduction-to-state-machines-using-xstate-with-kyle-shevlin-2019-11-13"
+          >
+            Get Ticket
+          </OutboundLink>
+        </div>
       </Container>
     </div>
   )
