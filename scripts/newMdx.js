31509de25a7b4dfa7bc8019325c820651b58ca3c
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2020-02-21 08:27:22 -0800

    DRY up scripts for making new posts

--- scripts/newMdx.js
+++ scripts/newMdx.js
@@ -1,49 +1,11 @@
-/*
- * This script is designed to create the directory structure and necessary files
- * for a new blog post with MDX.
- *
- * Parameters:
- * dirName: string - A hyphenated directory name
- */
-
-const fs = require('fs')
-const path = require('path')
-
-const POSTS_PATH = path.resolve('./src/posts/published')
+const makePost = require('./makePost')
 const dirName = process.argv[2]
 
-if (!dirName) {
-  throw new Error('You need to supply a name for the new directory!')
+const frontmatterFormatter = frontmatter => {
+  return `
+${frontmatter}
+import { FootnoteMarker as Marker } from '../../../components/Footnotes'
+`
 }
 
-try {
-  fs.mkdirSync(path.resolve(POSTS_PATH, dirName))
-
-  const now = new Date()
-  const year = now.getFullYear()
-  const month = (now.getMonth() + 1).toString().padStart(2, '0')
-  const day = now
-    .getDate()
-    .toString()
-    .padStart(2, '0')
-
-  const title = dirName
-    .split('-')
-    .map(word => {
-      const [firstLetter, ...rest] = word
-      return `${firstLetter.toUpperCase()}${rest.join('')}`
-    })
-    .join(' ')
-
-  const frontmatter = `
----
-date: '${year}-${month}-${day}'
-slug: '${dirName}'
-title: '${title}'
----
-`.trim()
-
-  fs.writeFileSync(path.resolve(POSTS_PATH, dirName, 'index.mdx'), frontmatter)
-} catch (err) {
-  console.log(err)
-}
+makePost(dirName, 'index.mdx', frontmatterFormatter)
