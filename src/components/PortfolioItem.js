f069fa97bb3218654bc69bf7877f1d77759bf86a
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2020-03-16 23:16:25 -0700

    add theme to portfolio item

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { css } from '@emotion/core'
+import { useTheme } from 'emotion-theming'
 import { Link } from 'gatsby'
 import { rem, rgba } from 'polished'
-import { BREAKPOINTS, COLORS } from '../constants'
+import { BREAKPOINTS } from '../constants'
 import { bs } from '../shevy'
 import { createMediaQuery } from '../utils'
 
@@ -27,68 +28,69 @@ const linkStyles = css`
   }
 `
 
-const titleStyles = css`
-  width: 100%;
-  color: ${COLORS.white};
-  font-size: ${rem(35)};
-  line-height: 1.2;
-  text-align: center;
-  padding-left: ${bs(0.5)};
-  padding-right: ${bs(0.5)};
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  opacity: 0;
-  transition: opacity 0.3s ease;
+const titleStyles = theme => ({
+  width: '100%',
+  color: theme.colors.background,
+  fontSize: rem(35),
+  lineHeight: 1.2,
+  textAlign: 'center',
+  paddingLeft: bs(0.5),
+  paddingRight: bs(0.5),
+  position: 'absolute',
+  top: '50%',
+  transform: 'translateY(-50%)',
+  opacity: 0,
+  transition: 'opacity 0.3s ease',
 
-  ${createMediaQuery(BREAKPOINTS.alpha)} {
-    font-size: ${rem(25)};
+  [createMediaQuery(BREAKPOINTS.alpha)]: {
+    fontSize: rem(25)
+  },
+
+  [createMediaQuery(BREAKPOINTS.delta)]: {
+    fontSize: rem(30)
   }
+})
 
-  ${createMediaQuery(BREAKPOINTS.delta)} {
-    font-size: ${rem(30)};
-  }
-`
+export default function PortfolioItem({
+  shortTitle,
+  slug,
+  squareImage,
+  title
+}) {
+  const theme = useTheme()
 
-class PortfolioItem extends Component {
-  render() {
-    const { shortTitle, slug, squareImage, title } = this.props
-
-    return (
-      <div
-        css={{
-          overflow: 'hidden',
-          position: 'relative',
-          width: '100%',
-          '&:hover': {
-            '[data-item-link]:before': {
-              backgroundColor: rgba(COLORS.teal, 0.5),
-              top: 0,
-              bottom: 0
-            },
-            '[data-title]': {
-              opacity: 1
-            }
+  return (
+    <div
+      css={{
+        overflow: 'hidden',
+        position: 'relative',
+        width: '100%',
+        '&:hover': {
+          '[data-item-link]:before': {
+            backgroundColor: rgba(theme.colors.accent, 0.5),
+            top: 0,
+            bottom: 0
+          },
+          '[data-title]': {
+            opacity: 1
           }
+        }
+      }}
+    >
+      <img
+        css={{
+          display: 'block',
+          height: 'auto',
+          width: '100%'
         }}
-      >
-        <img
-          css={{
-            display: 'block',
-            height: 'auto',
-            width: '100%'
-          }}
-          alt={title}
-          src={squareImage.publicURL}
-        />
-        <Link css={linkStyles} to={`portfolio/${slug}`} data-item-link>
-          <div css={titleStyles} data-title>
-            {shortTitle ? shortTitle : title}
-          </div>
-        </Link>
-      </div>
-    )
-  }
+        alt={title}
+        src={squareImage.publicURL}
+      />
+      <Link css={linkStyles} to={`portfolio/${slug}`} data-item-link>
+        <div css={titleStyles} data-title>
+          {shortTitle ? shortTitle : title}
+        </div>
+      </Link>
+    </div>
+  )
 }
-
-export default PortfolioItem
