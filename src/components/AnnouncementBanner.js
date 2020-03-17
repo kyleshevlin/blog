b4e55e008d621f7e3d881d0513e826fe4b5493b2
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2020-03-16 22:56:41 -0700

    add theme to announcement banner

--- src/components/AnnouncementBanner.js
+++ src/components/AnnouncementBanner.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import { useTheme } from 'emotion-theming'
 import Container from './Container'
-import { COLORS, FONTS } from '../constants'
 import { bs } from '../shevy'
 
 export default function AnnouncementBanner({ content = '' }) {
+  const theme = useTheme()
+
   return (
     <div
       css={{
-        backgroundColor: COLORS.teal,
-        color: COLORS.white,
-        fontFamily: FONTS.catamaran,
+        backgroundColor: theme.colors.accent,
+        color: theme.colors.background,
+        fontFamily: theme.fonts.catamaran,
         paddingTop: bs(0.5),
         paddingBottom: bs(0.5)
       }}
