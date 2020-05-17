89d41ad4d5e710bb4101fd84aa3b3055d48d30aa
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2020-05-17 09:54:56 -0700

    update LinkButton

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -1,16 +1,9 @@
 import React from 'react'
 import { buttonStyles } from './Button'
-import { bs } from '../shevy'
-
-const linkButtonStyles = theme => ({
-  ...buttonStyles(theme),
-  paddingTop: bs(0.125),
-  paddingBottom: bs(0.125),
-})
 
 export default function LinkButton({ children, href }) {
   return (
-    <a css={linkButtonStyles} href={href}>
+    <a css={buttonStyles} href={href}>
       {children}
     </a>
   )
