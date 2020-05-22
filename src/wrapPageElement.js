5ec7a97c1d483da47f1807096b034e76e915e1db
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2020-05-21 21:54:57 -0700

    Revert "move FootnoteMarker to mdx provider component"
    This reverts commit bbd149b33f4a842e800dfdaa695e30374af350f7.

--- src/wrapPageElement.js
+++ src/wrapPageElement.js
@@ -1,23 +1,17 @@
 import React from 'react'
-import { MDXProvider } from '@mdx-js/react'
-import { FootnoteMarker as Marker } from './components/Footnotes'
 import Layout from './components/Layout'
 import CoursesProvider from './components/CoursesProvider'
 import { FootnotesProvider } from './components/Footnotes'
 import ThemeProvider from './components/ThemeProvider'
 
-const shortcodes = { Marker }
-
 const wrapPageElement = ({ element, props }) => {
   return (
     <ThemeProvider>
-      <MDXProvider components={shortcodes}>
-        <CoursesProvider>
-          <FootnotesProvider>
-            <Layout {...props}>{element}</Layout>
-          </FootnotesProvider>
-        </CoursesProvider>
-      </MDXProvider>
+      <CoursesProvider>
+        <FootnotesProvider>
+          <Layout {...props}>{element}</Layout>
+        </FootnotesProvider>
+      </CoursesProvider>
     </ThemeProvider>
   )
 }
