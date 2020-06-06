2ec82f5d9a9efcf026aaad5ca721bab077360baa
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2020-06-06 15:21:26 -0700

    create functionality that updates css custom properties with theme change

--- src/components/ThemeProvider.js
+++ src/components/ThemeProvider.js
@@ -211,12 +211,101 @@ const NEXT_THEME = {
 
 const getInitialTheme = () => 'blackout'
 
+const makeDefaultCSSVars = colors => ({
+  '--colors-background': colors.background,
+  '--colors-text': colors.text,
+  '--colors-accent': colors.accent,
+  '--colors-offset': colors.offset,
+  '--colors-offsetMore': colors.offsetMore,
+  '--colors-error': colors.error,
+  '--fonts-catamaran': 'Catamaran, sans-serif',
+  '--components-announcementBanner-background': 'var(--colors-accent)',
+  '--components-announcementBanner-text': 'var(--colors-background)',
+  '--components-announcementBanner-links-text': '#f2f2f2',
+  '--components-announcementBanner-links-hover-text':
+    'var(--colors-background)',
+  '--components-button-background': 'var(--colors-accent)',
+  '--components-button-text': 'var(--colors-background)',
+  '--components-footer-background': 'var(--colors-text)',
+  '--components-footer-text': 'var(--colors-background)',
+  '--components-newsletterCTA-background': 'var(--colors-accent)',
+  '--components-newsletterCTA-text': 'var(--colors-background)',
+  '--components-newsletterCTA-errorBox-background': 'var(--colors-error)',
+  '--components-newsletterCTA-errorBox-text': 'var(--colors-background)',
+  '--components-newsletterCTA-inputs-background': 'var(--colors-background)',
+  '--components-newsletterCTA-inputs-text': 'var(--colors-text)',
+  '--components-newsletterCTA-inputs-placeholderText':
+    'var(--colors-offsetMore)',
+  '--components-newsletterCTA-submitButton-background': 'var(--colors-offset)',
+  '--components-newsletterCTA-submitButton-text': 'var(--colors-accent)',
+  '--components-newsletterCTA-submitButton-hover-background':
+    'var(--colors-background)',
+  '--components-newsletterCTA-submitButton-hover-text': 'var(--colors-accent)',
+  '--components-newsletterCTA-successBox-background': 'var(--colors-offset)',
+  '--components-newsletterCTA-successBox-text': 'var(--colors-text)',
+  '--components-pagination-normal-background': 'var(--colors-accent)',
+  '--components-pagination-normal-text': 'var(--colors-background)',
+  '--components-pagination-active-background': 'var(--colors-offset)',
+  '--components-pagination-active-text': 'var(--colors-accent)',
+  '--components-searchBox-text': 'var(--colors-text)',
+  '--components-searchError-background': 'var(--colors-error)',
+  '--components-searchError-text': 'var(--colors-background)',
+})
+
+const LIGHT_VARS = makeDefaultCSSVars(LIGHT_COLORS)
+
+const DARK_VARS = {
+  ...makeDefaultCSSVars(DARK_COLORS),
+  '--components-announcementBanner-text': 'var(--colors-text)',
+  '--components-announcementBanner-links-hover-text': 'var(--colors-text)',
+  '--components-button-text': 'var(--colors-text)',
+  '--components-footer-background': 'var(--colors-offset)',
+  '--components-footer-text': 'var(--colors-text)',
+  '--components-newsletterCTA-text': 'var(--colors-text)',
+  '--components-newsletterCTA-errorBox-text': 'var(--colors-text)',
+  '--components-newsletterCTA-inputs-background': 'var(--colors-text)',
+  '--components-newsletterCTA-inputs-text': 'var(--colors-background)',
+  '--components-newsletterCTA-submitButton-background': '#f2f2f2',
+  '--components-newsletterCTA-submitButton-hover-background':
+    'var(--colors-text)',
+  '--components-pagination-normal-text': 'var(--colors-text)',
+  '--components-pagination-active-text': 'var(--colors-text)',
+}
+
+const BLACKOUT_VARS = {
+  ...makeDefaultCSSVars(BLACKOUT_COLORS),
+  '--components-announcementBanner-links-text': 'var(--colors-offsetMore)',
+  '--components-footer-background': 'var(--colors-offset)',
+  '--components-footer-text': 'var(--colors-text)',
+  '--components-newsletterCTA-errorBox-text': 'var(--colors-text)',
+  '--components-newsletterCTA-inputs-background': 'var(--colors-text)',
+  '--components-newsletterCTA-inputs-text': 'var(--colors-background)',
+}
+
+const makeCSSVarsString = obj =>
+  Object.entries(obj).reduce((acc, [key, value]) => {
+    return acc + `${key}:${value};`
+  }, '')
+
+const THEME_TO_CSS_VARS = {
+  blackout: BLACKOUT_VARS,
+  dark: DARK_VARS,
+  light: LIGHT_VARS,
+}
+
+const getCSSVars = (theme = 'light') =>
+  makeCSSVarsString(THEME_TO_CSS_VARS[theme]).replace(/\s/g, '')
+
 export default function ThemeProvider({ children }) {
   const [theme, setTheme] = React.useState(getInitialTheme)
 
   const rotateTheme = () => {
     setTheme(NEXT_THEME[theme])
   }
+
+  React.useEffect(() => {
+    document.documentElement.setAttribute('style', getCSSVars(theme))
+  }, [theme])
 
   return (
     <EmotionThemeProvider theme={{ ...THEMES[theme], rotateTheme }}>
