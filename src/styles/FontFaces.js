77e0bf4ed90ae96c2e00f2d74eca772cecc0c59b
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2020-06-08 08:47:26 -0700

    minor tweak to font faces

--- src/styles/FontFaces.js
+++ src/styles/FontFaces.js
@@ -9,53 +9,53 @@ import catamaranThinWoff2 from '../fonts/catamaran-thin-webfont.woff2'
 import DroidSerifWoff from '../fonts/DroidSerif-Regular-webfont.woff'
 import DroidSerifBoldWoff from '../fonts/DroidSerif-Bold-webfont.woff'
 
-const GlobalStyles = () => (
-  <Global
-    styles={css`
+export default function FontFaces() {
+  return (
+    <Global
+      styles={css`
       @font-face {
         font-family: 'Droid Serif';
         font-style: normal;
         font-weight: normal;
-        src: url('${DroidSerifWoff}') format('woff');
         font-display: swap;
+        src: url('${DroidSerifWoff}') format('woff');
       }
 
       @font-face {
         font-family: 'Droid Serif';
         font-style: normal;
         font-weight: bold;
-        src: url('${DroidSerifBoldWoff}') format('woff');
         font-display: swap;
+        src: url('${DroidSerifBoldWoff}') format('woff');
       }
 
       @font-face {
         font-family: 'Catamaran';
         font-style: normal;
         font-weight: normal;
+        font-display: swap;
         src: url('${catamaranRegularWoff2}') format('woff2'),
              url('${catamaranRegularWoff}') format('woff');
-        font-display: swap;
       }
 
       @font-face {
         font-family: 'Catamaran';
         font-style: normal;
         font-weight: 100;
+        font-display: swap;
         src: url('${catamaranThinWoff2}') format('woff2'),
              url('${catamaranThinWoff}') format('woff');
-        font-display: swap;
       }
 
       @font-face {
         font-family: 'Catamaran';
         font-style: normal;
         font-weight: 700;
+        font-display: swap;
         src: url('${catamaranBoldWoff2}') format('woff2'),
              url('${catamaranBoldWoff}') format('woff');
-        font-display: swap;
       }
     `}
-  />
-)
-
-export default GlobalStyles
+    />
+  )
+}
