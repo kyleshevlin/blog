34fb2998221b0b422702c350781e56337cd14baa
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-04-03 20:13:21 -0700

    add sortable snippet columns

--- src/pages/snippets.js
+++ src/pages/snippets.js
@@ -4,9 +4,70 @@ import Seo from '../components/Seo'
 import AddedValue from '../components/AddedValue'
 import { bs } from '../shevy'
 import { mq, getNodes } from '../utils'
+import { Machine } from 'xstate'
+import { useMachine } from '@xstate/react'
+
+const sortsMachine = Machine({
+  id: 'sorts',
+  initial: 'nameAsc',
+  states: {
+    nameAsc: {
+      on: {
+        CLICK_NAME: 'nameDesc',
+        CLICK_CATEGORY: 'categoryAsc',
+      },
+    },
+    nameDesc: {
+      on: {
+        CLICK_NAME: 'nameAsc',
+        CLICK_CATEGORY: 'categoryAsc',
+      },
+    },
+    categoryAsc: {
+      on: {
+        CLICK_NAME: 'nameAsc',
+        CLICK_CATEGORY: 'categoryDesc',
+      },
+    },
+    categoryDesc: {
+      on: {
+        CLICK_NAME: 'nameAsc',
+        CLICK_CATEGORY: 'categoryAsc',
+      },
+    },
+  },
+})
+
+const comparators = {
+  nameAsc: (a, b) => {
+    if (a.frontmatter.name < b.frontmatter.name) return -1
+    if (a.frontmatter.name > b.frontmatter.name) return 1
+    return 0
+  },
+  nameDesc: (a, b) => {
+    if (a.frontmatter.name > b.frontmatter.name) return -1
+    if (a.frontmatter.name < b.frontmatter.name) return 1
+    return 0
+  },
+  categoryAsc: (a, b) => {
+    if (a.frontmatter.category < b.frontmatter.category) return -1
+    if (a.frontmatter.category > b.frontmatter.category) return 1
+    return 0
+  },
+  categoryDesc: (a, b) => {
+    if (a.frontmatter.category > b.frontmatter.category) return -1
+    if (a.frontmatter.category < b.frontmatter.category) return 1
+    return 0
+  },
+}
+
+const safeSort = comparator => array => [...array].sort(comparator)
 
 export default function Snippets({ data }) {
+  const [state, send] = useMachine(sortsMachine)
+  const comparator = comparators[state.value]
   const snippets = getNodes(data.snippets)
+  const sortedSnippets = safeSort(comparator)(snippets)
   const joshLink = (
     <a href="https://www.joshwcomeau.com/snippets/">Josh W. Comeau's blog</a>
   )
@@ -46,13 +107,21 @@ export default function Snippets({ data }) {
             }}
           >
             <Row>
-              <HeadingCell>Name</HeadingCell>
+              <HeadingCell onClick={() => send('CLICK_NAME')}>
+                <span>Name</span>
+                {state.matches('nameAsc') && <Arrow />}
+                {state.matches('nameDesc') && <Arrow flip />}
+              </HeadingCell>
               <HeadingCell>Description</HeadingCell>
-              <HeadingCell>Category</HeadingCell>
+              <HeadingCell onClick={() => send('CLICK_CATEGORY')}>
+                <span>Category</span>
+                {state.matches('categoryAsc') && <Arrow />}
+                {state.matches('categoryDesc') && <Arrow flip />}
+              </HeadingCell>
             </Row>
           </thead>
           <tbody css={{ display: 'table-row-group' }}>
-            {snippets.map(snippet => {
+            {sortedSnippets.map(snippet => {
               const { id, frontmatter } = snippet
               const { category, description, name, slug } = frontmatter
 
@@ -123,19 +192,23 @@ function Cell({ children }) {
   )
 }
 
-function HeadingCell({ children }) {
+function HeadingCell({ children, onClick }) {
+  const clickableProps = onClick ? { onClick, role: 'button' } : {}
+
   return (
     <th
       css={{
         fontFamily: 'var(--fonts-catamaran)',
         fontWeight: 'bold',
+        cursor: onClick ? 'pointer' : 'default',
 
         [mq.bravo]: {
           display: 'table-cell',
-          padding: `${bs(0.5)} ${bs(0.75)}`,
+          padding: `${bs(0.5)} ${bs(0.5)}`,
           textAlign: 'left',
         },
       }}
+      {...clickableProps}
     >
       {children}
     </th>
@@ -154,6 +227,25 @@ function Label({ children }) {
       }}
     >
       {children}
+    </span>
+  )
+}
+
+function Arrow({ width = 8, flip = false }) {
+  const height = width
+
+  const path = flip ? 'M0,15 15,15 8,0Z' : 'M0,0, 15,0, 8,15Z'
+
+  return (
+    <span css={{ marginLeft: bs(0.25) }}>
+      <svg
+        width={width}
+        height={height}
+        viewBox="0 0 15 15"
+        fill="currentColor"
+      >
+        <path d={path} />
+      </svg>
     </span>
   )
 }
