04f28a81c1dc61b6a458702910c339deb7bbab57
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-08-30 15:06:40 -0700

    updates shevy

--- src/shevy.js
+++ src/shevy.js
@@ -1,12 +1,12 @@
-import Shevy from 'shevyjs'
+import createShevy from 'shevyjs'
 
-const shevy = new Shevy({
+const shevy = createShevy({
   baseFontSize: '1rem',
   baseLineHeight: 1.8,
-  baseFontScale: [2.4414, 1.9531, 1.5625, 1.25, 1, 1],
-  addMarginBottom: true,
-  proximity: true,
-  proximityFactor: 0.85,
+  fontScale: [2.4414, 1.9531, 1.5625, 1.25, 1, 1],
+  includeMarginBottom: true,
+  precision: 4,
+  proximity: 0.85,
 })
 
 export const bs = shevy.baseSpacing
