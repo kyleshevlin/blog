1f0839f9b0bc3eb3ddf5bbcd70828f3490999755
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2022-05-27 13:21:45 -0700

    ain't no one donating anyways

--- src/components/FinishedReading.js
+++ src/components/FinishedReading.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import shevy, { bs } from '../shevy'
 import BeardStrokes from './BeardStrokes'
-import KofiLogo from './KofiLogo'
-import LinkButton from './LinkButton'
 import Share from './Share'
-import ShiftBy from './ShiftBy'
 import Margin from './Margin'
-import { mq } from '../utils'
 
 export default function FinishedReading({ beardStrokeKey, slug, title }) {
   return (
@@ -19,11 +15,9 @@ export default function FinishedReading({ beardStrokeKey, slug, title }) {
         <AfterOption label="Like">
           <BeardStrokes slug={beardStrokeKey} />
         </AfterOption>
+
         <AfterOption label="Share">
           <Share slug={slug} title={title} />
-        </AfterOption>
-        <AfterOption label="Support">
-          <Kofi />
         </AfterOption>
       </AfterOptionWrap>
     </div>
@@ -59,40 +53,5 @@ function AfterOption({ children, label }) {
       </div>
       <div css={{ display: 'flex', alignItems: 'center' }}>{children}</div>
     </>
-  )
-}
-
-function Kofi() {
-  return (
-    <div css={{ textAlign: 'center', width: '100%' }}>
-      <Margin bottom={0.25}>
-        <div
-          css={{
-            fontFamily: 'var(--fonts-catamaran)',
-          }}
-        >
-          Was this post valuable to you? Make a donation to show&nbsp;it
-        </div>
-      </Margin>
-      <LinkButton
-        href="https://ko-fi.com/kyleshevlin"
-        overrideStyles={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: '100%',
-          height: 56,
-        }}
-      >
-        <span css={{ fontSize: shevy.h4.fontSize }}>Make a Donation</span>
-        <div css={{ display: 'none', [mq.alpha]: { display: 'block' } }}>
-          <Margin vert={0.25} left={0.5}>
-            <ShiftBy y={2}>
-              <KofiLogo width={40} />
-            </ShiftBy>
-          </Margin>
-        </div>
-      </LinkButton>
-    </div>
   )
 }
