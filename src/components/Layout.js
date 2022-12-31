4c49a1f1342d10d56787e8c29a47214f809290c4
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2022-12-30 17:24:47 -0800

    update layout lib to latest

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Margin } from '@kyleshevlin/layout'
+import { Flex, Margin } from '@kyleshevlin/layout'
 import Container from './Container'
 import Footer from './Footer'
 import { FootnoteDisplay } from './Footnotes'
@@ -11,13 +11,7 @@ export default function Layout({ children }) {
     <>
       <AllGlobalStyles />
 
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          minHeight: '100vh',
-        }}
-      >
+      <Flex direction="column" style={{ minHeight: '100vh' }}>
         <Header />
 
         <div css={{ flexGrow: 1 }}>
@@ -27,7 +21,7 @@ export default function Layout({ children }) {
         <Margin top={2}>
           <Footer />
         </Margin>
-      </div>
+      </Flex>
       <FootnoteDisplay />
     </>
   )
