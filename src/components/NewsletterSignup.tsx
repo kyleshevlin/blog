8bca4c005c9fa54b59f1478b55246724c3267d99
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2024-01-19 16:27:07 -0800

    tweaks

--- src/components/NewsletterSignup.tsx
+++ src/components/NewsletterSignup.tsx
@@ -5,14 +5,14 @@ import { isEmail } from '../utils'
 
 export function NewsletterSignup() {
   return (
-    <div className="rounded bg-accent p-8 text-white">
-      <div className="flex flex-col gap-4">
-        <div className="font-sans text-2xl text-white">
+    <div className="rounded bg-accent p-8 text-white xl:p-12">
+      <div className="flex flex-col gap-4 xl:gap-6">
+        <div className="font-sans text-2xl text-white xl:text-3xl">
           Sign up for my newsletter
         </div>
 
         <div>
-          Let's chat some more about TypeScript, React, and frontend web
+          Let&apos;s chat some more about TypeScript, React, and frontend web
           development. Unsubscribe at any&nbsp;time.
         </div>
 
